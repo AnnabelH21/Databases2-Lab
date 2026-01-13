41c7c323a20eee7091136bd32b2ac3a609fee772
AnnabelH21 <annabel.heberle@online.de>
2026-01-13 01:33:23 +0100

    fix: change in DatabseCOnnector

--- shopadministration/src/main/java/com/gameshop/DatabaseConnector.java
+++ shopadministration/src/main/java/com/gameshop/DatabaseConnector.java
@@ -1,7 +1,9 @@
 package com.gameshop;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,21 +18,19 @@ public class DatabaseConnector {
     private static String PASSWORD;
 
     static {
-        try (FileInputStream fis = new FileInputStream("config.properties")) {
+        File file = new File("config.properties");
+        try (FileInputStream fis = new FileInputStream(file)) {
             props.load(fis);
+            URL = props.getProperty("db.url");
+            USER = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
+            
         } catch (IOException e) {
             System.err.println("config.properties not found" + System.getProperty("user.dir"));
             e.printStackTrace();
         }
-
-        // Connection values for the msql server
-        URL = props.getProperty("db.url");
-        USER = props.getProperty("db.user");
-        PASSWORD = props.getProperty("db.password");
     }
-
-    
-
+ 
     public static Connection getConnection() throws SQLException {
         try {
             
